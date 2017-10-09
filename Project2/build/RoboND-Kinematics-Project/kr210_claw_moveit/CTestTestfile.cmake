2a48e6f7fd4ba811317d9d0825dc0fb3d026f23a
aminghafari <amin.ghafari@berkeley.edu>
2017-10-09 01:03:31 -0700

    Forward Kinematic

---
+++ Project2/build/RoboND-Kinematics-Project/kr210_claw_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Robotics_Udacity_NanoDegree/Project2/src/RoboND-Kinematics-Project/kr210_claw_moveit
+# Build directory: /home/robond/Robotics_Udacity_NanoDegree/Project2/build/RoboND-Kinematics-Project/kr210_claw_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
