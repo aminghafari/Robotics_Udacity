2d38aa514606f5ddcd870e6cc0ed751ef764b584
aminghafari <amin.ghafari@berkeley.edu>
2017-10-09 01:15:53 -0700

    add p2

---
+++ Project2/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robond/Robotics_Udacity_NanoDegree/Project2/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robond/Robotics_Udacity_NanoDegree/Project2/devel;/opt/ros/kinetic".split(';') if "/home/robond/Robotics_Udacity_NanoDegree/Project2/devel;/opt/ros/kinetic" != "" else []
