37da3a147dce3f23e98f24035f420427b43ce6ef
aminghafari <amin.ghafari@berkeley.edu>
2017-10-09 01:14:18 -0700

    remove p2

--- Project2/build/RoboND-Kinematics-Project/gazebo_grasp_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Robotics_Udacity_NanoDegree/Project2/src/RoboND-Kinematics-Project/gazebo_grasp_plugin
-# Build directory: /home/robond/Robotics_Udacity_NanoDegree/Project2/build/RoboND-Kinematics-Project/gazebo_grasp_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Project2/build/RoboND-Kinematics-Project/kuka_arm/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/robond/Robotics_Udacity_NanoDegree/Project2/devel/include".split(';') if "/home/robond/Robotics_Udacity_NanoDegree/Project2/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "kuka_arm"
-PROJECT_SPACE_DIR = "/home/robond/Robotics_Udacity_NanoDegree/Project2/devel"
-PROJECT_VERSION = "0.0.0"
