298af6ef4bfcd3d43dbaf982db00438c3d7766df
aminghafari <amin.ghafari@berkeley.edu>
2017-10-13 01:26:51 -0700

    forwardK

--- Project2/src/RoboND-Kinematics-Project/IK_debug.py
+++ Project2/src/RoboND-Kinematics-Project/IK_debug.py
@@ -63,14 +63,46 @@ def test_code(test_case):
     ## 
 
     ## Insert IK code here!
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+         [req.poses[x].orientation.x, req.poses[x].orientation.y,
+         req.poses[x].orientation.z, req.poses[x].orientation.w])
+     
+    ### Your IK code here 
+    # Compensate for rotation discrepancy between DH parameters and Gazebo
+    #
+    rx, ry, rz = symbols('rx ry rz')
+    R_x = Matrix([[       1,        0,        0],
+                  [       0,  cos(rx), -sin(rx),],
+                  [       0,  sin(rx),  cos(rx),]])
+
+    R_y = Matrix([[  cos(ry),        0, sin(ry)],
+                  [        0,        1,       0],
+                  [ -sin(ry),        0, cos(ry)]])
+
+    R_z = Matrix([[ cos(rz), -sin(rz),        0],
+                  [ sin(rz),  cos(rz),        0],
+                  [       0,        0,        1]])
+
+    R_corr = R_z.subs(rz, radians(180))*R_y.subs(ry, -radians(90))
+
+    R_G = R_z*R_y*R_x*R_corr
+    R_G = R_G.subs({'rx' : roll, 'ry' : pitch, 'rz': yaw})
+
+
+    Gr = Matrix([[px],[py],[pz]])
     
+    WC = Gr - 0.303*R_G[:,2]
+
     theta1 = 0
     theta2 = 0
     theta3 = 0
     theta4 = 0
     theta5 = 0
     theta6 = 0
-
     ## 
     ########################################################################################
     
@@ -86,83 +118,39 @@ def test_code(test_case):
     alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
     #   
     # Create Modified DH parameters
-    s = {alpha0:     0, a0:     0, d1: 0.75,
-         alpha1: -pi/2, a1:  0.35, d2:    0, q2: q2-pi/2.,
-         alpha2:     0, a2:  1.25, d3:    0,
-         alpha3: -pi/2, a3:-0.054, d4:  1.5,
-         alpha4:  pi/2, a4:     0, d5:    0,
-         alpha5:  pi/2, a5:     0, d6:    0,
-         alpha6:     0, a6:     0, d7: 0.303, q7: 0}
+    s = {alpha0:     0, a0:     0, d1: 0.75,alpha1: -pi/2., a1:  0.35, d2:    0, q2: q2-pi/2., alpha2:     0, a2:  1.25, d3:    0, alpha3: -pi/2., a3:-0.054, d4:  1.5, alpha4:  pi/2, a4:     0, d5:    0, alpha5:  pi/2., a5:     0, d6:    0, alpha6:     0, a6:     0, d7: 0.303, q7: 0}
     #            
     # Define Modified DH Transformation matrix
     #
-    T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
-                   [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-                   [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-                   [                   0,                   0,            0,               1]])
-    T0_1 = T0_1.subs(s)
+    def T_Mat(q, d, a, alpha):
+        T = Matrix([[             cos(q),            -sin(q),            0,              a],
+                       [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                       [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                       [                   0,                   0,            0,               1]])
 
-    T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
-                   [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-                   [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-                   [                   0,                   0,            0,               1]])
-    T0_1 = T0_1.subs(s)
+        return T
 
-    T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
-                   [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-                   [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-                   [                   0,                   0,            0,               1]])
-    T2_3 = T2_3.subs(s)
+    T0_1 = T_Mat(q1, d1, a0, alpha0).subs(s)
+    T1_2 = T_Mat(q2, d2, a1, alpha1).subs(s)
+    T2_3 = T_Mat(q3, d3, a2, alpha2).subs(s)
+    T3_4 = T_Mat(q4, d4, a3, alpha3).subs(s)
+    T4_5 = T_Mat(q5, d5, a4, alpha4).subs(s)
+    T5_6 = T_Mat(q6, d6, a5, alpha5).subs(s)
+    T6_G = T_Mat(q7, d7, a6, alpha6).subs(s)
 
-    T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-                   [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-                   [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-                   [                   0,                   0,            0,               1]])
-    T3_4 = T3_4.subs(s)
-
-    T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-                   [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-                   [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-                   [                   0,                   0,            0,               1]])
-    T4_5 = T4_5.subs(s)
-
-    T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-                   [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-                   [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-                   [                   0,                   0,            0,               1]])
-    T5_6 = T5_6.subs(s)
-
-    T6_G = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
-                   [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
-                   [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
-                   [                   0,                   0,            0,               1]])
-    T6_G = T6_G.subs(s)
     #
     # Create individual transformation matrices
     #
     #
-    T0_2 = simplify( T0_1 * T1_2)
-    T0_3 = simplify( T0_2 * T2_3)
-    T0_4 = simplify( T0_3 * T3_4)
-    T0_5 = simplify( T0_4 * T4_5)
-    T0_6 = simplify( T0_5 * T5_6)
-    T0_G = simplify( T0_6 * T6_G)
+    T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
     
-    s1 = {q1:theta1, q2:theta2, q3:theta3, q4:theta4, q5:theta5, q6:theta6}
-
-    T1 = simplify( T0_1.subs(s1))
-    T2 = simplify( T0_2.subs(s1))
-    T3 = simplify( T0_3.subs(s1))
-    T4 = simplify( T0_4.subs(s1))
-    T5 = simplify( T0_5.subs(s1))
-    T6 = simplify( T0_6.subs(s1))
-    TG = simplify( T0_G.subs(s1))
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = T6[0:3,3] # <--- Load your calculated WC values in this array
-    your_ee = TG[0:3,3] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = WC # <--- Load your calculated WC values in this array
+    your_ee = WC # <--- Load your calculated end effector value from your forward kinematics
+    print(your_wc)
     ########################################################################################
 
     ## Error analysis
