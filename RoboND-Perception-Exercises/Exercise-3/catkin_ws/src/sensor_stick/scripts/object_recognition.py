e32aead97f05ac1e9f8d8660cb286060e16d8030
aminghafari <amin.ghafari@berkeley.edu>
2017-10-29 02:47:01 -0700

    feature extraction

--- RoboND-Perception-Exercises/Exercise-3/catkin_ws/src/sensor_stick/scripts/object_recognition.py
+++ RoboND-Perception-Exercises/Exercise-3/catkin_ws/src/sensor_stick/scripts/object_recognition.py
@@ -128,7 +128,7 @@ def pcl_callback(pcl_msg):
         # Publish a label into RViz
         label_pos = list(white_cloud[pts_list[0]])
         label_pos[2] += .4
-        #object_markers_pub.publish(make_label(label,label_pos, index))
+        object_markers_pub.publish(make_label(label,label_pos, index))
 
         # Add the detected object to the list of detected objects.
         do = DetectedObject()
@@ -152,8 +152,8 @@ if __name__ == '__main__':
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
-    object_markers_pub = rospy.Publisher("/object_markers", PointCloud2, queue_size=1)
-    detected_objects_pub = rospy.Publisher("/detected_objects", PointCloud2, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
     # TODO: Load Model From disk
     model = pickle.load(open('model.sav', 'rb'))
